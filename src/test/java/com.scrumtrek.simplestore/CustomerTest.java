0734cb3113b1dc7189935f5f737289384c4ecc32
Evgeny Prudnikov <Evgeny.Prudnikov@t-systems.ru>
2015-10-22 19:43:33 +0300

    add a heap of tests

--- src/test/java/com.scrumtrek.simplestore/CustomerTest.java
+++ src/test/java/com.scrumtrek.simplestore/CustomerTest.java
@@ -19,6 +19,10 @@ public class CustomerTest {
             "\tGladiator\t15.0\n" +
             "Amount owed is 24.5\n" +
             "You earned 4 frequent renter points.";
+    public static final double REGULAR_MOVIE_COST = 2.;
+    public static final double NEW_RELEASE_COST = 3.;
+    public static final double CHILDREN_MOVIE_COST = 1.5;
+    public static final double EXTRA__MOVIE_COST = 1.5;
 
     private Customer customer;
 
@@ -50,28 +54,70 @@ public class CustomerTest {
     public void testRegularMovieFor1Day() {
         customer.addRental(new Rental(new Movie(TITLE, PriceCodes.Regular), 1));
         String statement = customer.Statement();
-        assertStatement(statement, 2.);
+        assertStatement(statement, REGULAR_MOVIE_COST);
     }
 
     @Test
     public void testRegularMovieFor2Day() {
         customer.addRental(new Rental(new Movie(TITLE, PriceCodes.Regular), 2));
         String statement = customer.Statement();
-        assertStatement(statement, 2.);
+        assertStatement(statement, REGULAR_MOVIE_COST);
     }
 
     @Test
     public void testRegularMovieFor3Day() {
         customer.addRental(new Rental(new Movie(TITLE, PriceCodes.Regular), 3));
         String statement = customer.Statement();
-        assertStatement(statement, 3.5);
+        assertStatement(statement, REGULAR_MOVIE_COST + EXTRA__MOVIE_COST);
     }
 
     @Test
     public void testRegularMovieFor4Day() {
         customer.addRental(new Rental(new Movie(TITLE, PriceCodes.Regular), 4));
         String statement = customer.Statement();
-        assertStatement(statement, 5);
+        assertStatement(statement, REGULAR_MOVIE_COST + EXTRA__MOVIE_COST * 2);
+    }
+
+    @Test
+    public void testNewReleaseFor1Day() {
+        customer.addRental(new Rental(new Movie(TITLE, PriceCodes.NewRelease), 1));
+        String statement = customer.Statement();
+        assertStatement(statement, NEW_RELEASE_COST);
+    }
+
+    @Test
+    public void testNewReleaseFor5Day() {
+        customer.addRental(new Rental(new Movie(TITLE, PriceCodes.NewRelease), 5));
+        String statement = customer.Statement();
+        assertStatement(statement, NEW_RELEASE_COST * 5);
+    }
+
+    @Test
+    public void testChildrenMovieFor1Day() {
+        customer.addRental(new Rental(new Movie(TITLE, PriceCodes.Childrens), 1));
+        String statement = customer.Statement();
+        assertStatement(statement, CHILDREN_MOVIE_COST);
+    }
+
+    @Test
+    public void testChildrenMovieFor3Day() {
+        customer.addRental(new Rental(new Movie(TITLE, PriceCodes.Childrens), 3));
+        String statement = customer.Statement();
+        assertStatement(statement, CHILDREN_MOVIE_COST);
+    }
+
+    @Test
+    public void testChildrenMovieFor4Day() {
+        customer.addRental(new Rental(new Movie(TITLE, PriceCodes.Childrens), 4));
+        String statement = customer.Statement();
+        assertStatement(statement, EXTRA__MOVIE_COST);
+    }
+
+    @Test
+    public void testChildrenMovieFor6Day() {
+        customer.addRental(new Rental(new Movie(TITLE, PriceCodes.Childrens), 5));
+        String statement = customer.Statement();
+        assertStatement(statement, EXTRA__MOVIE_COST * 2);
     }
 
     public void assertStatement(String statement, double amount) {
