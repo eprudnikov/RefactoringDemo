db1625ad22dae1d4784fefb2973f1f7ddf6dc4f1
Evgeny Prudnikov <Evgeny.Prudnikov@t-systems.ru>
2015-11-03 22:01:20 +0300

    rework frequent points calculation in functional way

--- context.md
+++ context.md
@@ -1,4 +1,4 @@
 *In this file I describe context of the project, i.e. what precondition I assume.*
 
 #Current situation
-Class Customer became more readable and testable. Let's refactor it in more OOD way.
+Let's try to rework strategies in function way.
--- src/main/java/com/scrumtrek/simplestore/PriceCodes.java
+++ src/main/java/com/scrumtrek/simplestore/PriceCodes.java
@@ -3,9 +3,9 @@ package com.scrumtrek.simplestore;
 import com.scrumtrek.simplestore.pricing.*;
 
 public enum PriceCodes {
-    REGULAR(new MoreDaysMoreMoneyStrategy(2., 1.5, 2), new TwoPointsStrategy(1, 1, 0)),
-    NEW_RELEASE(new NoFixedPriceStrategy(3.), new TwoPointsStrategy(1, 2, 1)),
-    CHILDREN(new FixPriceThresholdStrategy(1.5, 1.5, 3), new TwoPointsStrategy(1, 1, 0));
+    REGULAR(new MoreDaysMoreMoneyStrategy(2., 1.5, 2), (daysRented) -> 1),
+    NEW_RELEASE(new NoFixedPriceStrategy(3.), (daysRented) -> daysRented > 1 ? 2 : 1),
+    CHILDREN(new FixPriceThresholdStrategy(1.5, 1.5, 3), (daysRented) -> 1);
 
     private PriceStrategy priceStrategy;
     private FrequentPointsCalculationStrategy frequentPointsStrategy;
