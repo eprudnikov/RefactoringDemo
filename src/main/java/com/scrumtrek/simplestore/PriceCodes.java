f58ce9d75a14096e36b7d55b53f382f303e7d8fa
Evgeny Prudnikov <Evgeny.Prudnikov@t-systems.ru>
2015-11-03 22:07:37 +0300

    rework amount calculation in functional way

--- src/main/java/com/scrumtrek/simplestore/PriceCodes.java
+++ src/main/java/com/scrumtrek/simplestore/PriceCodes.java
@@ -3,9 +3,11 @@ package com.scrumtrek.simplestore;
 import com.scrumtrek.simplestore.pricing.*;
 
 public enum PriceCodes {
-    REGULAR(new MoreDaysMoreMoneyStrategy(2., 1.5, 2), (daysRented) -> 1),
-    NEW_RELEASE(new NoFixedPriceStrategy(3.), (daysRented) -> daysRented > 1 ? 2 : 1),
-    CHILDREN(new FixPriceThresholdStrategy(1.5, 1.5, 3), (daysRented) -> 1);
+    REGULAR(daysRented -> daysRented <= 2 ? 2. : 2 + (daysRented - 2) * 1.5,
+            daysRented -> 1),
+    NEW_RELEASE(daysRented -> daysRented * 3., daysRented -> daysRented > 1 ? 2 : 1),
+    CHILDREN(daysRented -> daysRented <= 3 ? 1.5 : (daysRented - 3) * 1.5,
+            daysRented -> 1);
 
     private PriceStrategy priceStrategy;
     private FrequentPointsCalculationStrategy frequentPointsStrategy;
